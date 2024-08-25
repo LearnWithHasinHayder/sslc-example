86fdc72d49c7e70809f9ab0584b049548ce7b242
Hasin Hayder <countdraculla@gmail.com>
2024-08-25 21:36:09 +0530

    laravel with ssl commerze integration

--- database/migrations/2024_08_25_153812_create_orders_table.php
+++ database/migrations/2024_08_25_153812_create_orders_table.php
@@ -13,14 +13,16 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 255)->nullable();
+            $table->foreignId("user_id")->constrained()->castadeOnDelete();
+            $table->string('name')->nullable();
             $table->string('email', 30)->nullable();
             $table->string('phone', 20)->nullable();
             $table->double('amount')->nullable();
             $table->text('address')->nullable();
             $table->string('status', 10)->nullable();
-            $table->string('transaction_id', 255)->nullable();
+            $table->string('transaction_id')->nullable();
             $table->string('currency', 20)->nullable();
+            $table->timestamps();
         });
     }
 
